510090177e24a9a4612774978b9768e171bf3492
aquinoacordero <aquinoacordero@eniac33.danielcastelao.org>
2014-12-04 09:04:18 +0100

    commit_Final

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -1,3 +1,4 @@
+
 package examencod;
 
 public class ExamenCOD {
@@ -6,30 +7,26 @@ public class ExamenCOD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int num = 11;
-        if (metodoMio(num)) {
-            System.out.println("Es numero primo");
-        } else {
-            System.out.println("NO es numero primo");
-        }
-
-        for (int numX = 2; numX < 1000; numX++) {
-            if (metodoMio(numX)) {
-                System.out.print(numX + " ");
-            }
-        }
-    }
-
-    public static boolean metodoMio(int variable) {
-        int num = 2;
-        boolean situacion = true;
-        while ((situacion) && (num != variable)) {
-            if (variable % num == 0) {
-                situacion = false;
-            }
-            num++;
-        }
-        return situacion;
-    }
-
+ int iN = 11;
+            	if (metodoMio(iN))
+System.out.println("Es numero primo");
+else
+ System.out.println("NO es numero primo");           	 
+          	 
+for (int x=2;x<1000;x++) {
+if (metodoMio(x))
+System.out.print(x + " ");                         	 
+}         	 
+	}
+public static boolean metodoMio(int variable){
+int j = 2;
+boolean aux=true;
+while ((aux) && (j!=variable)){
+if (variable % j == 0)
+aux = false;
+j++;
+}
+return aux;
+  } 	 
+    
 }
