b479860767727079d1062b2628ff5a21b549426a
aquinoacordero <aquinoacordero@eniac33.danielcastelao.org>
2014-12-04 09:03:15 +0100

    commit_Variables

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -6,30 +6,30 @@ public class ExamenCOD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int iN = 11;
-        if (metodoMio(iN)) {
+        int num = 11;
+        if (metodoMio(num)) {
             System.out.println("Es numero primo");
         } else {
             System.out.println("NO es numero primo");
         }
 
-        for (int x = 2; x < 1000; x++) {
-            if (metodoMio(x)) {
-                System.out.print(x + " ");
+        for (int numX = 2; numX < 1000; numX++) {
+            if (metodoMio(numX)) {
+                System.out.print(numX + " ");
             }
         }
     }
 
     public static boolean metodoMio(int variable) {
-        int j = 2;
-        boolean aux = true;
-        while ((aux) && (j != variable)) {
-            if (variable % j == 0) {
-                aux = false;
+        int num = 2;
+        boolean situacion = true;
+        while ((situacion) && (num != variable)) {
+            if (variable % num == 0) {
+                situacion = false;
             }
-            j++;
+            num++;
         }
-        return aux;
+        return situacion;
     }
 
 }
